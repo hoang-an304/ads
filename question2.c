df656ffe3cf1f31a46bb838228add00f19decd8b
unknown <anprolong12@gmail.com>
2025-04-02 16:14:52 +0700

    lam bai nay voi

---
+++ question2.c
@@ -0,0 +1,6 @@
+// implement clock data structure:
+// structure to store hours(with minute and second)
+// initialize clock variable with values: hour, minute and second
+// increase clock variable by 1s
+// calculate difference of 2 clocks
+// main function to test all above
